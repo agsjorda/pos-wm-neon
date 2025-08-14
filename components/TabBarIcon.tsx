8021380d67768461d6f72e27d108ef3ce21abbbc
agsjorda <42771722+agsjorda@users.noreply.github.com>
2025-08-14 13:12:04 +0800

    -added classname utility function -removed unused styles -optimized TabBarIcon component

--- components/TabBarIcon.tsx
+++ components/TabBarIcon.tsx
@@ -1,15 +1,26 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { StyleSheet } from 'react-native';
+import { View } from 'react-native';
+import { cn } from '../lib/utils';
 
-export const TabBarIcon = (props: {
+export const TabBarIcon = ({
+  name,
+  color,
+  size = 28,
+  className,
+}: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
+  size?: number;
+  className?: string;
 }) => {
-  return <FontAwesome size={28} style={styles.tabBarIcon} {...props} />;
+  return (
+    <View className={cn('items-center justify-center', className)}>
+      <FontAwesome 
+        name={name} 
+        color={color} 
+        size={size} 
+        style={{ marginBottom: -3 }}
+      />
+    </View>
+  );
 };
-
-export const styles = StyleSheet.create({
-  tabBarIcon: {
-    marginBottom: -3,
-  },
-});
